2868f3824d8db715806b0fffd9950e5a6dc4b808
rameshrvr <rameshrvr@outlook.com>
2017-09-17 23:30:16 +0530

    small_change

---
+++ spec/models/expense_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+
+RSpec.describe Expense, :type => :model do
+  # Create sample data
+  subject(:expense) { Expense.create(name: 'Test', weight: '2500', month: 'Apr') }
+
+  it "Verify nil value in name column" do
+  	name = Expense.new(name: nil)
+  	expect(name).to_not be_valid
+  end
+
+  it "Verify nil value in weight column" do
+  	weight = Expense.new(weight: nil)
+  	expect(weight).to_not be_valid
+  end
+
+  it "Verify nil value in month column" do
+  	month = Expense.new(month: nil)
+  	expect(month).to_not be_valid
+  end
+
+  it "Verify the name in expense table" do
+    expect(expense.name.eql? 'Test')
+  end
+
+  it "Verify the amount in expense table" do
+  	expect(expense.weight.eql? '2500')
+  end
+
+  it "Verify the month in expense table" do
+  	expect(expense.month.eql? 'Apr')
+  end
+  it "is not valid without a description"
+  it "is not valid without a start_date"
+  it "is not valid without a end_date"
+end
