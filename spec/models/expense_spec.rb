b716f1ab4de4277baa0a1ba800e757ebb0ace9f4
rameshrvr <rameshrvr@outlook.com>
2017-09-18 10:44:39 +0530

    'typo_fix'

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -5,18 +5,18 @@ RSpec.describe Expense, :type => :model do
   subject(:expense) { Expense.create(name: 'Test', weight: '2500', month: 'Apr') }
 
   it "Verify nil value in name column" do
-  	name = Expense.new(name: nil)
-  	expect(name).to_not be_valid
+    name = Expense.new(name: nil)
+    expect(name).to_not be_valid
   end
 
   it "Verify nil value in weight column" do
-  	weight = Expense.new(weight: nil)
-  	expect(weight).to_not be_valid
+    weight = Expense.new(weight: nil)
+    expect(weight).to_not be_valid
   end
 
   it "Verify nil value in month column" do
-  	month = Expense.new(month: nil)
-  	expect(month).to_not be_valid
+    month = Expense.new(month: nil)
+    expect(month).to_not be_valid
   end
 
   it "Verify the name in expense table" do
@@ -24,10 +24,32 @@ RSpec.describe Expense, :type => :model do
   end
 
   it "Verify the amount in expense table" do
-  	expect(expense.weight.to_s).to eq('2500')
+    expect(expense.weight.to_s).to eq('2500')
   end
 
   it "Verify the month in expense table" do
-  	expect(expense.month).to eq('Apr')
+    expect(expense.month).to eq('Apr')
+  end
+
+  it "Update weight value and verify" do
+    expense.update(weight: '3700')
+    expect(expense.weight.to_s).to eq('3700')
+  end
+
+  it "Update name value and verify" do
+    expense.update(name: 'Test Update')
+    expect(expense.name).to eq('Test Update')
+  end
+
+  it "Update month value and verify" do
+    expense.update(month: 'Dec')
+    expect(expense.month).to eq('Dec')
+  end
+
+  it "Update all fields and verify" do
+    expense.update(name: 'Last Update', weight: '4700', month: 'Aug')
+    expect(expense.name).to eq('Last Update')
+    expect(expense.weight.to_s).to eq('4700')
+    expect(expense.month).to eq('Aug')
   end
 end
--- spec/models/month_spec.rb
+++ spec/models/month_spec.rb
@@ -19,4 +19,27 @@ RSpec.describe Month, :type => :model do
   it "Verify the result in month table" do
   	expect(month.result).to eq('Sample description')
   end
+
+  it "Verify sum of all expenses for a particular month" do
+  	Expense.create(name: 'Test2', weight: '200', month: 'May')
+  	Expense.create(name: 'Test3', weight: '3500', month: 'May')
+  	expenses_sum = Expense.where(month: 'May').sum(:weight)
+  	expect(expenses_sum.to_s).to eq('3700')
+  end
+
+  it "Update result value and verify" do
+    month.update(result: 'Updated description')
+    expect(month.result).to eq('Updated description')
+  end
+
+  it "Update name value and verify" do
+    month.update(name: 'Update Month')
+    expect(month.name).to eq('Update Month')
+  end
+
+  it "Update all fields and verify" do
+    month.update(name: 'Last Update', result: 'Final description')
+    expect(month.name).to eq('Last Update')
+    expect(month.result).to eq('Final description')
+  end
 end
